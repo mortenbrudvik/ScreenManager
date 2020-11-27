82990b43fa677cbfd352ea6fdb44cdf62d3e8c55
Morten Brudvik <morten@brudvik.com>
2020-11-27 18:14:56 +0100

    missing from previous commit

--- ApplicationCore/Interfaces/IScreenManager.cs
+++ ApplicationCore/Interfaces/IScreenManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace ApplicationCore.Interfaces
 {
-    public interface IScreenService
+    public interface IScreenManager
     {
         event EventHandler Changed;
      
